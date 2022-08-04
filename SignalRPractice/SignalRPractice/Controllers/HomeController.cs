784a4c0f7e80be2d693762a55d85907411380888
muradav <muradav@code.edu.az>
2022-08-04 20:42:54 +0400

    feat: last lesson written again

--- SignalRPractice/SignalRPractice/Controllers/HomeController.cs
+++ SignalRPractice/SignalRPractice/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using SignalRPractice.DAL;
 using SignalRPractice.Models;
@@ -15,17 +16,16 @@ namespace SignalRPractice.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-        private static string CurUserName;
-        private readonly AppDbContext _context;
+        private readonly IHubContext<ChatHub> _hubContext;
 
 
 
-        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext context)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IHubContext<ChatHub> hubContext)
         {
             _logger = logger;
             _userManager = userManager;
             _signInManager = signInManager;
-            _context = context;
+            _hubContext = hubContext;
         }
         public IActionResult Index()
         {
@@ -34,16 +34,15 @@ namespace SignalRPractice.Controllers
   
         public IActionResult Chat()
         {
-            List<AppUser> users = _userManager.Users.Where(u => u.ConnectId != null).ToList();
-            ViewBag.ChatUsers = users;
-            return View();
+            List<AppUser> users = _userManager.Users.ToList();
+            return View(users);
         }
 
         public async Task<IActionResult> CreateUser()
         {
-            var result = await _userManager.CreateAsync(new AppUser { UserName = "_rashid", Email = "rashidrma@code.edu.az", isOnline = false, Fullname = "Elgun" }, "12345@Ca");
+            var result = await _userManager.CreateAsync(new AppUser { UserName = "_rashid", Email = "rashidrma@code.edu.az", isOnline = false, Fullname = "Rashid" }, "12345@Ca");
             var result1 = await _userManager.CreateAsync(new AppUser { UserName = "_farid", Email = "feridtb@code.edu.az", isOnline = false, Fullname = "Ferid" }, "12345@Ca");
-            var result2 = await _userManager.CreateAsync(new AppUser { UserName = "_murad", Email = "muradav@code.edu.az", isOnline = false, Fullname = "Xalid" }, "12345@Ca");
+            var result2 = await _userManager.CreateAsync(new AppUser { UserName = "_murad", Email = "muradav@code.edu.az", isOnline = false, Fullname = "Murad" }, "12345@Ca");
             var result3 = await _userManager.CreateAsync(new AppUser { UserName = "_huseyn", Email = "huseynfh@code.edu.az", isOnline = false, Fullname = "Huseyn" }, "12345@Ca");
             return Ok("User Created");
 
@@ -57,21 +56,21 @@ namespace SignalRPractice.Controllers
         public async Task<IActionResult> Login(LoginVM model)
         {
             var user = _userManager.FindByNameAsync(model.Username).Result;
-            CurUserName = user.Email;
             var result = await _signInManager.PasswordSignInAsync(user, model.Password, true, true);
-            user.isOnline = true;
-            _context.SaveChanges();
-            ViewBag.UserName = user.Fullname;
-            return RedirectToAction("chat", "home");
+            return RedirectToAction("Index");
 
         }
-        public async Task<IActionResult> Logout()
+        public IActionResult Logout()
         {
-            AppUser appUser = await _userManager.FindByEmailAsync(CurUserName);
-            appUser.isOnline = false;
-            _context.SaveChanges();
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index");
+            _signInManager.SignOutAsync();
+            return RedirectToAction("index");
+        }
+
+        public async Task<IActionResult> PrivateSend(string id)
+        {
+            AppUser appUser = await _userManager.FindByIdAsync(id);
+            await _hubContext.Clients.Client(appUser.ConnectId).SendAsync("PrivateMessage");
+            return RedirectToAction("chat");
         }
 
     }
